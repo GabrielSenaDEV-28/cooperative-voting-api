2534dd007bf6e52ca3104983036752db0d8d00f6
Gabriel Sena <roodes28@gmail.com>
2026-09-03 19:40:45 -0300

    chore: initialize project

---
+++ src/main/java/com/gabrielsena/cooperative_voting_api/CooperativeVotingApiApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielsena.cooperative_voting_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperativeVotingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperativeVotingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielsena/cooperative_voting_api/CooperativeVotingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielsena.cooperative_voting_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperativeVotingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
