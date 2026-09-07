fe777d0291e76acf6b6fab73f6a441c9a1cbbea6
Gabriel Sena <roodes28@gmail.com>
2026-09-07 03:17:42 -0300

    chore: add business event logging

--- src/main/java/com/gabrielsena/cooperative_voting_api/domain/topic/VotingTopicService.java
+++ src/main/java/com/gabrielsena/cooperative_voting_api/domain/topic/VotingTopicService.java
@@ -3,9 +3,11 @@ package com.gabrielsena.cooperative_voting_api.domain.topic;
 import com.gabrielsena.cooperative_voting_api.domain.topic.dto.CreateVotingTopicRequest;
 import com.gabrielsena.cooperative_voting_api.domain.topic.dto.CreateVotingTopicResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class VotingTopicService {
@@ -15,6 +17,11 @@ public class VotingTopicService {
     public CreateVotingTopicResponse create(CreateVotingTopicRequest request) {
         VotingTopic topic = new VotingTopic(request.title());
         VotingTopic savedTopic = votingTopicRepository.save(topic);
+
+        log.info(
+                "Voting topic created: topicId={}",
+                savedTopic.getId()
+        );
 
         return new CreateVotingTopicResponse(
                 savedTopic.getId(),
